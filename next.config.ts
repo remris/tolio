369b685f05ed8b1cf61b9db42affb482a8639107
Christian R <remmele.chris@gmail.com>
2026-04-23 22:50:53 +0200

    fix: serverExternalPackages for __dirname issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   turbopack: {},
+  serverExternalPackages: ['stripe', 'bcryptjs', 'qrcode'],
 };
 
 export default nextConfig;
